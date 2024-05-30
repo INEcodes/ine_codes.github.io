00365a6a27a69abce8cbbe5a2d8050212f79e212
Abhay Singh Chauhan <92663561+INEcodes@users.noreply.github.com>
2024-05-30 15:20:55 +0530

    tailwind css + react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-        <h1>MERN Portfolio</h1>
+    <div className="App flex h-screen items-center justify-center">
+        <h1 className='text-5x1'>MERN Portfolio</h1>
     </div>
   );
 }
